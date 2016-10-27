122ca1099b8ed1b4dad57867c15a28b0f8762ca6
shalantor <protipas2@hotmail.com>
2016-10-27 16:20:41 +0300

    Add menu before game start

--- start.py
+++ start.py
@@ -15,7 +15,57 @@ def tps(orologio,fps):
 
 
 pygame.init()
+
+#Show menu for login
+pygame.mouse.set_visible(0)
 screen = pygame.display.set_mode(SCREEN_SIZE, FULLSCREEN, 32)
+clock = pygame.time.Clock()
+background = pygame.Surface(screen.get_size()).convert()
+width,height = screen.get_size()
+font = pygame.font.Font(None,height // 10)
+
+#render texts
+playText = font.render("PLAY",1,(255,255,255))
+loadText = font.render("LOAD",1,(255,255,255))
+optionsText = font.render("OPTIONS",1,(255,255,255))
+exitText = font.render("EXIT",1,(255,255,255))
+
+#Get rectangles for text
+playPos = playText.get_rect()
+loadPos = loadText.get_rect()
+optionsPos = optionsText.get_rect()
+exitPos = exitText.get_rect()
+
+#Set position of texts
+playPos.centerx = background.get_rect().centerx
+loadPos.centerx = background.get_rect().centerx
+optionsPos.centerx = background.get_rect().centerx
+exitPos.centerx = background.get_rect().centerx
+
+playPos.centery = background.get_rect().centery - 3 * playPos.height
+loadPos.centery = background.get_rect().centery - 1 * loadPos.height
+optionsPos.centery = background.get_rect().centery + 1 * optionsPos.height
+exitPos.centery = background.get_rect().centery + 3 * exitPos.height
+
+#blit to background
+background.blit(playText,playPos)
+background.blit(loadText,loadPos)
+background.blit(optionsText,optionsPos)
+background.blit(exitText,exitPos)
+
+#blit to screen
+screen.blit(background,(0,0))
+pygame.display.flip()
+leaveLoop = False
+
+while not leaveLoop:
+    for event in pygame.event.get():
+        if event.type == KEYDOWN and event.key == K_ESCAPE or event.type == QUIT:
+            leaveLoop = True
+    screen.blit(background,(0,0))
+    time_spent = tps(clock, FPS)
+
+"""Now set variables for gameplay """
 screen_rect = screen.get_rect()
 background = pygame.image.load("world/background2.jpg").convert_alpha()
 background_rect = background.get_rect()
@@ -23,15 +73,12 @@ level = Level("level/level.txt")
 level.create_level(0,0)
 world = level.world
 player = level.player
-pygame.mouse.set_visible(0)
 
 camera = Camera(screen, player.rect, level.get_size()[0], level.get_size()[1])
 all_sprite = level.all_sprite
-
-clock = pygame.time.Clock()
-
 up = down = left = right = False
 x, y = 0, 0
+
 while True:
 
     for event in pygame.event.get():
@@ -58,7 +105,6 @@ while True:
             right = False
 
     asize = ((screen_rect.w // background_rect.w + 1) * background_rect.w, (screen_rect.h // background_rect.h + 1) * background_rect.h)
-    bg = pygame.Surface(asize)
 
     for x in range(0, asize[0], background_rect.w):
         for y in range(0, asize[1], background_rect.h):
