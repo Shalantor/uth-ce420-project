d739176c334b6ddaa20de170da9a159a91322a3f
shalantor <protipas2@hotmail.com>
2016-10-30 18:08:53 +0200

    Database setup now inserts start profiles

--- databaseUtils.py
+++ databaseUtils.py
@@ -2,6 +2,9 @@
 import sqlite3
 
 DATABASE = 'random.db'
+START_LEVEL = 1
+START_LIVES = 3
+START_COINS = 0
 
 #Create table for database
 def setupDatabase():
@@ -13,5 +16,24 @@ def setupDatabase():
                                 lives integer,
                                 coins integer,
                                 PRIMARY KEY(id))''')
+    cursor.execute('SELECT * FROM Profiles')
+    if cursor == None:
+        #now fill with four profiles
+        startProfiles = [ (0,'EMPTY',START_LEVEL,START_LIVES,START_COINS),
+                          (1,'EMPTY',START_LEVEL,START_LIVES,START_COINS),
+                          (2,'EMPTY',START_LEVEL,START_LIVES,START_COINS),
+                          (3,'EMPTY',START_LEVEL,START_LIVES,START_COINS),]
+        cursor.executemany('INSERT INTO Profiles VALUES (?,?,?,?,?)',startProfiles)
+
+    connection.commit()
+    connection.close()
+
+
+#Enter new user (will be called for new game)
+def enterProfile(userId,name):
+    connection = sqlite3.connect(DATABASE)
+    cursor = connection.cursor()
+    data = (userId,name,START_LEVEL,START_LIVES,START_COINS,userId)
+    cursor.execute('UPDATE Profiles SET id=?,name=?,level=?,lives=?,coins=? WHERE id=?',data)
     connection.commit()
     connection.close()
